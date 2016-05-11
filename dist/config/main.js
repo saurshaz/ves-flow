4a05bb06d5ead27c04b9b9547d668d6440f639e4
Saurabh SHarma <saurshaz@gmail.com>
2016-05-11 20:55:18 +0530

    push in dist folder for the gh-pages

---
+++ dist/config/main.js
@@ -0,0 +1,11 @@
+/*
+** This file is the entry into the app
+** List all different components needed here
+** 
+*/
+
+// import '../lib/mixins/eh-mixin.js'
+// import '../lib/mixins/riot-creator-mixin.js'
+console.log(' app kickstarted ')
+let path = require('path')
+require('./router')
