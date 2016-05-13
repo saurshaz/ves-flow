c2b64380687355deb9c22e442afd2d06ebe7b117
Saurabh SHarma <saurshaz@gmail.com>
2016-05-13 16:04:47 +0530

    transported common code to a simple StoreWatcher Mixin

--- client/components/mixins/storex.js
+++ client/components/mixins/storex.js
@@ -1,38 +1,29 @@
+'use strict'
+
+import events from '../../event-manager/events'
+import store from '../../event-manager/store'
+import PubSub from '../../scripts/pubsub'
+
 module.exports = {
   // init method is a special one which can initialize
   // the mixin when it's loaded to the tag and is not
   // accessible from the tag its mixed in
-  init: function () {
-    this.on('updated', function () { console.log('Updated!') })
-  },
-
-  getOpts: function () {
-    return this.opts
-  },
-
-  log: function (obj) {
-    console.dir('me again logging')
-  },
-
-  setOpts: function (opts, update) {
-    this.opts = opts
-    if (!update) this.update()
-    return this
-  },
-
-  updateMe: function (changeArr) {
+  init: function (stores) {
+    // this.on('updated', function () { console.log('Updated!') })
     let self = this
-    console.log('****** setting changed state *********', changeArr)
-    for (var i in changeArr) {
-      var obj = changeArr[i]
-      var key = Object.keys(obj)[0]
-      self[key] = obj[key]
-    }
-    self.update()
-  },
-  initState: function (state) {
-    let self = this
-    console.log('****** setting initial state *********', state)
-    self.update()
+    self.state = store.init()
+    self._ = self.state
+
+    self.on('mount', function () {
+      // todo :: make this move into a mixin which takes in an array 
+      self.stores.map((item, i) => {
+        PubSub.subscribe(item + '_updated', (data) => {
+          console.log(' update data >>> ', data)
+          self['_'][data.module][data.key] = data.val
+          self.update()
+        })
+      })
+      events.setupEvents(self)
+    })
   }
 }
