b664a2d113fb080aded9f931808fb8c2eef41d7b
Saurabh SHarma <saurshaz@gmail.com>
2016-05-13 14:10:00 +0530

    Code refactoring :: better event handling

--- client/event-manager/events.js
+++ client/event-manager/events.js
@@ -1,6 +1,26 @@
 'use strict;'
+
+import handlers from './handlers'
+import store from './store'
+
 //  TODO :: PIECE#1 EVENTS MAPPING CONFIG
 module.exports = {
+  setupEvents: (context) => {
+    // riot.mount('*')
+    let domain = context.opts.domain
+    let page = context.opts.page
+
+    if (domain && page) {
+      for (let idx in this[page]) {
+        let event_json = this[page][idx]
+        let handler = event_json.handler
+        context.root.querySelector(context.root.name + ' ' + event_json.selector).addEventListener(event_json.event, handlers[page][handler].bind(self._, event_json.passedValues, store, (err, result) => {
+          context.update()
+          console.log('err -> ', err, ' result-> ', context._)
+        }))
+      }
+    }
+  },
   login: [
     {
       selector: 'button#submitLogin',
